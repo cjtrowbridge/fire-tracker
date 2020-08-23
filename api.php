205ff9933318ecd43a89cd83c50ed383cd77e1eb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2020-08-22 18:09:06 -0700

    Update api.php

--- api.php
+++ api.php
@@ -1,8 +1,11 @@
 <?php
 
-$CacheFilename = 'cache/'.date('Y-m-d H:00').'.json';
+$CacheFilename = 'cache/'.date('Y-m-d').'.json';
 $APIURL = 'https://www.fire.ca.gov/umbraco/api/IncidentApi/List?';
-if(file_exists($CacheFilename)){
+if(
+  file_exists($CacheFilename) &&
+  (filemtime ($CacheFilename)>(time()-60*60))
+){
   $Data = file_get_contents($CacheFilename);
 }else{
   $Data = file_get_contents($APIURL);
